ee1451c79f2a4c25cb943011f21f7fae505407c2
JC <jorge.e.colindres@gmail.com>
2013-11-17 12:08:04 -0500

    updated commenting for easier read throughout the main js file

--- chimpaxify.js
+++ chimpaxify.js
@@ -2,7 +2,9 @@
   $.fn.chimpaxify = function (options) {
     var form = this;
     var defaults = $.extend({
-      url: form.attr('action'), // takes the default Mailchimp form url from the action attribute
+
+      // takes the default Mailchimp form url from the action attribute
+      url: form.attr('action'),
       timeOut: 3500,
       delay: 4000,
       speed: 'slow',
@@ -10,29 +12,52 @@
       loader: true,
       successMessage: 'Success. A confirmation email has been sent your way.'
     }, options);
-    var jsonUrl = defaults.url.replace('/post?', '/post-json?').concat('&c=?'); // adds the appropriate json formatting to the url
-    var message = $('<div id="chimpaxifyMessage"></div>'); // setting up the message container
-    form.find('input[type=email]').attr('name', 'EMAIL'); // adds the (seemingly?) required EMAIL name attribute to the email input
+
+    // adds the appropriate json formatting to the url
+    var jsonUrl = defaults.url.replace('/post?', '/post-json?') + '&c=?';
+
+    // setting up the message container
+    var message = $('<div id="chimpaxifyMessage"></div>');
+
+    // adds the required EMAIL name attribute to the email input
+    form.find('input[type=email]').attr('name', 'EMAIL');
     $(form).append(message);
+
     form.on('submit', function (e) {
-      e.preventDefault(); // Prevent the form from refreshing the page -- it's AJAX after all!
+
+      // prevent the form from refreshing the page -- it's AJAX after all!
+      e.preventDefault();
+
+      // grab the data from the form and serialize it
       var dataSent = $(this).serialize();
       $.ajax({
         contentType: 'application/json',
-        dataType: 'jsonp', // Pulling from Mailchimps servers, so we use jsonp instead of regular json
+
+        // Pulling from Mailchimp's server, so we use jsonp instead of regular json
+        dataType: 'jsonp',
         url: jsonUrl,
         data: dataSent,
         timeout: defaults.timeOut,
         success: function (result) {
           if (result.result === 'success') {
+
+            // on success, show the success message with proper styling and animation
             $(message).html(defaults.successMessage);
-            $(message).removeClass('chimpaxifyError').addClass('chimpaxifySuccess').slideDown(defaults.speed, defaults.easing);
+            $(message).removeClass('chimpaxifyError')
+                      .addClass('chimpaxifySuccess')
+                      .slideDown(defaults.speed, defaults.easing);
           } else {
+
+            // on error, show the error message with proper styling and animation
+            // using a simple regex to strip the error codes from the error message sent back from Mailchimp's
+            // server so it all shows up nicely as text
             $(message).html(result.msg.replace(/\d -/, ''));
-            $(message).removeClass('chimpaxifySuccess').addClass('chimpaxifyError').slideDown(defaults.speed, defaults.easing);
+            $(message).removeClass('chimpaxifySuccess')
+                      .addClass('chimpaxifyError')
+                      .slideDown(defaults.speed, defaults.easing);
           }
-          message.delay(defaults.delay).slideUp(defaults.speed, defaults.easing);
-          var messageCount = $('#chimpaxifyMessage').length;
+          message.delay(defaults.delay)
+                .slideUp(defaults.speed, defaults.easing);
         },
         error: function () {
           console.log('There seems to be a problem with your Chimpaxify request. Make sure your options are correct.');
@@ -43,7 +68,9 @@
           }
         },
         complete: function () {
-          form.removeClass('chimpaxifyLoading');
+          if (defaults.loader) {
+            form.removeClass('chimpaxifyLoading');
+          }
         }
       });
     });
